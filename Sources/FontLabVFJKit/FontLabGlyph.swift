310a6e1f6e78d0b517238635e3df5a2ba01dd2db
Łukasz Dziedzic <dziedzic@alfabety.pl>
2021-05-10 18:51:45 +0200

    no colorFlag

--- Sources/FontLabVFJKit/FontLabGlyph.swift
+++ Sources/FontLabVFJKit/FontLabGlyph.swift
@@ -11,7 +11,7 @@ import Foundation
 public class FontLabGlyph:Codable {
     public var name:String = ""
     public var unicode: String? = ""
-    public var colorFlag : Int? = nil
+    //public var colorFlag : Int? = nil
     public var layers:[FontLabLayer]? = nil
     
     
@@ -20,13 +20,13 @@ public class FontLabGlyph:Codable {
         case name
         case unicode
         case layers
-        case colorFlag
+        //case colorFlag
     }
     public required init(from decoder: Decoder) throws {
         
         let containr = try decoder.container(keyedBy: Keys.self)
         self.name = try containr.decode(String.self, forKey: .name)
-        self.colorFlag = try containr.decode(Int?.self, forKey: .colorFlag)
+        //self.colorFlag = try containr.decode(Int?.self, forKey: .colorFlag)
         self.layers = try containr.decode([FontLabLayer]?.self, forKey: .layers)
         print (self.name)
     }
