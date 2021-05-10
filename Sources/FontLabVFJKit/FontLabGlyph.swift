fe3dd05515d8fd5763f5d9ef54b4baf66cdc01d5
Łukasz Dziedzic <dziedzic@alfabety.pl>
2021-05-10 17:44:15 +0200

    bug search

--- Sources/FontLabVFJKit/FontLabGlyph.swift
+++ Sources/FontLabVFJKit/FontLabGlyph.swift
@@ -13,7 +13,25 @@ public class FontLabGlyph:Codable {
     public var unicode: String? = ""
     public var colorFlag : Int? = nil
     public var layers:[FontLabLayer]? = nil
+    
+    
+    //MARK: for debug only
+    enum Keys: CodingKey {
+        case name
+        case unicode
+        case layers
+        case colorFlag
+    }
+    public required init(from decoder: Decoder) throws {
+        
+        let containr = try decoder.container(keyedBy: Keys.self)
+        self.name = try containr.decode(String.self, forKey: .name)
+        self.colorFlag = try containr.decode(Int?.self, forKey: .colorFlag)
+        self.layers = containr.decode([FontLabLayer]?.self, forKey: .layers)
+        print (self.name)
+    }
 }
+
 
 
 extension FontLabGlyph: CustomStringConvertible {
